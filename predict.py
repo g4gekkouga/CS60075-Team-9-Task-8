511fefec52f784f6b863138fcaff8eff1433540d
Yeshwanth <yeshwanthkai@gmail.com>
2021-04-09 12:14:42 +0530

    second submission

--- predict.py
+++ predict.py
@@ -9,7 +9,8 @@ import torch.nn as nn
 import torch.optim as optim
 import torch._autograd_functions
 import torch.autograd as autograd
-import classifier
+from classifier import Classifier
+import sys
 
 if torch.cuda.is_available():
     device = torch.device("cuda")
@@ -22,7 +23,6 @@ chkpt_dir = "./checkpoints/"
 def get_prediction(entityModel, propertyModel, thresholdModel, test_data, test_quantIds):
     prediction_output = []
 
-    annotSet = 0
     for each_quant in test_quantIds:
         #enter quantity to results
         curr_batch = test_data.loc[test_data['uniqueId'] == each_quant]
@@ -36,11 +36,15 @@ def get_prediction(entityModel, propertyModel, thresholdModel, test_data, test_q
         entStartOffset  = list(curr_batch['entStartOffset'])
         entText = list(curr_batch['entText'])
         x_list = list(curr_batch['X'])
-        # quantity data
-        prediction_output.append(
-        [str(docId[0]), str(annotSet), "Quantity", str(quantStartOffSet[0]), str( int(quantStartOffSet[0]) + len(quantText[0])), str(annotSet)+"-1", str(quantText[0]), ""] ) 
+        index_list = list(curr_batch.index)
 
-        x = torch.stack(x_list,dim=0)
+        # print("{} {}".format(docId, qId))
+        # quantity data
+        pred_row = curr_batch.loc[index_list[0]]
+        prediction_output.append(
+        [pred_row['docId'], pred_row['annotSet'], "Quantity", pred_row['quantStartOffset'], pred_row['quantStartOffset'] + len(pred_row['quantText']), str(pred_row['annotSet'])+"-1", pred_row['quantText'], "{\"unit\": \"kg\"}"] ) 
+
+        x = torch.stack(x_list,dim=0).to(device)
         entityRelProb = entityModel(x)
         propertyRelProb = propertyModel(x)
         thresholdRelProb = thresholdModel(x)
@@ -52,7 +56,7 @@ def get_prediction(entityModel, propertyModel, thresholdModel, test_data, test_q
         entity_flag = 0 
         entity_threshold_prob_ratio = 0
         entity_index = -1
-        for i in range(len(curr_batch)):
+        for i in range(len(index_list)):
             if( entityRelList[i][0] < thresholdRelList[i][0] ):
                 continue
             
@@ -66,14 +70,17 @@ def get_prediction(entityModel, propertyModel, thresholdModel, test_data, test_q
         if entity_flag == 0:
             continue 
 
+        pred_row = curr_batch.loc[index_list[entity_index]]
         prediction_output.append(
-        [str(docId[entity_index]), str(annotSet), "MeasuredEntity", str(entStartOffset[entity_index]), str( int(entStartOffset[entity_index]) + len(entText[entity_index])), str(annotSet)+"-2", str(entText[entity_index])] ) 
+        [pred_row['docId'], pred_row['annotSet'], "MeasuredEntity", pred_row['entStartOffset'], pred_row['entStartOffset'] + len(pred_row['entText']), str(pred_row['annotSet'])+"-2", pred_row['entText'], "{\"HasQuantity\": "+ "\"{}".format(pred_row['annotSet']) +"-1\"}" ] ) 
 
+        propertyRelList.pop(entity_index)
+        index_list.pop(entity_index)
 
         property_flag = 0
         property_threshold_prob_ratio = 0
         property_index = -1
-        for i in range(len(curr_batch)):
+        for i in range(len(index_list)):
             if( propertyRelList[i][0] < thresholdRelList[i][0] ):
                 continue
 
@@ -84,8 +91,11 @@ def get_prediction(entityModel, propertyModel, thresholdModel, test_data, test_q
                 property_threshold_prob_ratio = curr_ratio
             
         if property_flag == 1:
+            pred_row = curr_batch.loc[index_list[property_index]]
             prediction_output.append(
-            [str(docId[property_index]), str(annotSet), "MeasuredProperty", str(entStartOffset[property_index]), str( int(entStartOffset[property_index]) + len(entText[property_index])), str(annotSet)+"-3", str(entText[property_index])] ) 
+            [pred_row['docId'], pred_row['annotSet'], "MeasuredProperty", pred_row['entStartOffset'], pred_row['entStartOffset'] + len(pred_row['entText']), str(pred_row['annotSet'])+"-3", pred_row['entText'], "{\"HasQuantity\": "+ "\"{}".format(pred_row['annotSet']) +"-1\"}" ] ) 
+
+        
 
     prediction_output = pd.DataFrame(prediction_output, columns = ["docId", "annotSet", "annotType", "startOffset", "endOffset", "annotId", "text", "other"])
     return prediction_output
@@ -94,18 +104,22 @@ entityRelClassifier = Classifier().to(device)
 propertyRelClassifier = Classifier().to(device)
 thresholdRelClassifier = Classifier().to(device)
 test_data = torch.load("test_data.pt")
+
 identifierList = []
 for _, row in test_data.iterrows():
     identifierList.append( str(row["docId"]) + "_" + str(row["quantId"]) )
 test_data["uniqueId"] = identifierList
 test_quantIds = list(set(test_data["uniqueId"]))
-# ent_filepath = chkpt_dir + "ent_model_" + str(int(start/1e6)) + "_" + str(epoch/100)+".pt"
-# prop_filepath = chkpt_dir + "prop_model_" + str(int(start/1e6)) + "_" + str(epoch/100)+".pt"
-# thresh_filepath = chkpt_dir + "thresh_model_" + str(int(start/1e6)) + "_" + str(epoch/100)+".pt"
 
-# entityRelClassifier.load_state_dict(torch.load(ent_filepath))
-# propertyRelClassifier.load_state_dict(torch.load(prop_filepath))
-# thresholdRelClassifier.load_state_dict(torch.load(thresh_filepath))
+ent_filepath = chkpt_dir + sys.argv[1]
+prop_filepath = chkpt_dir + sys.argv[2]
+thresh_filepath = chkpt_dir + sys.argv[3]
+
+entityRelClassifier.load_state_dict(torch.load(ent_filepath))
+propertyRelClassifier.load_state_dict(torch.load(prop_filepath))
+thresholdRelClassifier.load_state_dict(torch.load(thresh_filepath))
+
+# print("models loaded")
 
 prediction_df = get_prediction(entityRelClassifier, propertyRelClassifier, thresholdRelClassifier, test_data, test_quantIds )
 
